a3aea93feb3d7b3e40c2f6d07f638d027097fa59
Habeeb David <habeebdh1@gmail.com>
2021-11-25 01:13:44 +0100

    fix little error

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { useState } from 'react';
 import clientPromise from '../lib/mongodb';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+// import Button from '@mui/material/Button';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
