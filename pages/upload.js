fb9ddca51c41c5cf83fde14424252fe1c612e8f0
Habeeb David <habeebdh1@gmail.com>
2021-11-25 04:29:42 +0100

    Default homepage fix

--- pages/upload.js
+++ pages/upload.js
@@ -198,7 +198,7 @@ export default function Upload({ dept, port, deptName }) {
                 <br />
                 <Typography variant="subtitle2" gutterBottom>
                   You can check the home page to see your information{' '}
-                  <Link href="/profile">here.</Link>
+                  <Link href="/">here.</Link>
                 </Typography>
               </React.Fragment>
             ) : (
